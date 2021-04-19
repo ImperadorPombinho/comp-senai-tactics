ee95ba803f49d8672f6a3feeaf945098bd3a1564
ImperadorPombinho <pedrobarros0101@outlook.com>
2021-04-19 00:22:23 -0300

    tentando colocar legenda de coluna, mas nao deu muito certo

--- CST/src/aplicacao/UI.java
+++ CST/src/aplicacao/UI.java
@@ -58,7 +58,8 @@ public class UI {
             
             System.out.println();
         }
-        System.out.print(" ");
+        System.out.print("  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20");
+        System.out.println();
         
     }
     public static void printarTabuleiro(CSTpeca[][] pecas, int numeroLinhas, boolean[][] possiveisMovimentos){
