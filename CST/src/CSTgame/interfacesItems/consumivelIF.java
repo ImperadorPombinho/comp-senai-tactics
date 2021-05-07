10ba6594aa6fe525893f9e0c63c0de6d305086a4
ImperadorPombinho <pedrobarros0101@outlook.com>
2021-05-07 20:44:30 -0300

    Fazendo efeito pizza e camisa playstation

--- CST/src/CSTgame/interfacesItems/consumivelIF.java
+++ CST/src/CSTgame/interfacesItems/consumivelIF.java
@@ -1,5 +1,11 @@
 package CSTgame.interfacesItems;
 
+import CSTgame.CSTpeca;
+
 public interface consumivelIF {
-    
+    void efeitoFPyke(CSTpeca generica);
+    void efeitoPizza(CSTpeca generica);
+    void efeitoPototonime(CSTpeca generica);
+    void efeitoBeats(CSTpeca generica);
+    void efeitoPrevDoom(CSTpeca generica);
 }
--- CST/src/CSTgame/itemConsumivel.java
+++ CST/src/CSTgame/itemConsumivel.java
@@ -1,9 +1,57 @@
 package CSTgame;
 
+import CSTgame.interfacesItems.consumivelIF;
+
 /**
  * itemConsumivel
  */
-public class itemConsumivel {
+public class itemConsumivel implements consumivelIF{
+    private String nomeItem;
 
+    public String getNome() {
+        return nomeItem;
+    }
+
+    public itemConsumivel(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+
+    public void setNome(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+
+    @Override
+    public void efeitoFPyke(CSTpeca generica) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void efeitoPizza(CSTpeca generica) {
+        generica.setVida(generica.getVida() + 20);
+        
+    }
+
+    @Override
+    public void efeitoPototonime(CSTpeca generica) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void efeitoBeats(CSTpeca generica) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void efeitoPrevDoom(CSTpeca generica) {
+        // TODO Auto-generated method stub
+        
+    }
+    public void efeito(CSTpeca generica){
+        efeitoPizza(generica);
+    }
+    
     
 }
